e123b5dfb9e4410b82ddb9de385633715afe42ab
s821y9 <73866445+s821y9@users.noreply.github.com>
2022-12-07 16:31:48 -0500

    added database.db file

--- database/app.js
+++ database/app.js
@@ -4,7 +4,7 @@ const sqlite3 = require("sqlite3").verbose();
 
 //connect to DB
 
-const db = new sqlite3.Database('./test.db',sqlite3.OPEN_READWRITE,(err)=>{
+const db = new sqlite3.Database('./database.db',sqlite3.OPEN_READWRITE,(err)=>{
     if (err) return console.error(err.message);
 });
 
